c92a2a2b9b149371a540d9ec5bf29d78fc65d117
AMER-SOFT <imanefirek3@gmail.com>
2024-03-20 13:08:22 +0100

    add profile models to base user

--- account/models.py
+++ account/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+gender=(
+    ('Male','Male'),
+
+    ('Femlale','Femlale')
+)
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15,null=True,blank=True)
+    address = models.CharField(max_length=50,null=True,blank=True)
+    birthday = models.CharField(max_length=10,null=True,blank=True)
+    gender= models.CharField(choices=gender,max_length=10)
+
+
+    def __str__(self):
+        return str(self.user)
+    
+@receiver(post_save, sender=User)
+def created_user_profile(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(user=instance)
